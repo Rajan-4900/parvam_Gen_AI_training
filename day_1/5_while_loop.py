e90421f056ef8cf15062258311dd6e56734fed0a
Rajan <146510886+Rajan-4900@users.noreply.github.com>
2026-04-15 23:22:44 +0530

    while loop file added in day 1 file

---
+++ day_1/5_while_loop.py
@@ -0,0 +1,12 @@
+# while loop - repeat as long as a condition is true
+#syntax
+#ini---> initial value
+#while(condition):
+    #code to be executed
+
+num = int(input("Enter a number: "))
+ini = 0
+
+while(ini <= num):
+    print(ini,end=" ")
+    ini +=1
