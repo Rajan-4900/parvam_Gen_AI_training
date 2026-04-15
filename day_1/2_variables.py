5ec5750c9963a45254ad45533be021129fadfbf3
Rajan <146510886+Rajan-4900@users.noreply.github.com>
2026-04-15 22:56:48 +0530

    first commit and basic + simple hello world

---
+++ day_1/2_variables.py
@@ -0,0 +1,25 @@
+# 1. variable is a name that refers to a value
+
+a = 10
+b = 20
+c = "Hello"
+float = 3.14
+boolean = True
+
+# integer variable
+print("the value of a is",a)  # Output: 10
+print("type of number is",type(a))  # Output: <class 'int'>
+
+# string variable
+print("the value of string is",c)  # Output: Hello
+print("type of string is",type(c))  # Output: <class 'str'>
+
+# float variable
+print("the value of float is",float)  # Output: 3.14
+print("type of float is",type(float))  # Output: <class 'float'
+
+# boolean variable
+print("the value of boolean is",boolean)  # Output: True
+print("type of boolean is",type(boolean))  # Output: <class 'bool'>
+
+
