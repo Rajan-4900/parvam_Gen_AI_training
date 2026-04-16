d0fa85a9e60451319cec287acf82edefe7db7406
Rajan <146510886+Rajan-4900@users.noreply.github.com>
2026-04-16 11:51:19 +0530

    new file added in day 2 called train.txt file

---
+++ day_2/1_numpy.py
@@ -0,0 +1,22 @@
+# NumPy Basics numeric python
+
+list1 = [1, 2, 3, 4, 5]
+list2 = [i*2 for i in list1]
+print(list2)
+
+print()
+
+import numpy as np
+array1 = np.array(list1)
+print(array1*2)
+
+print()
+
+array1 = np.array(list1)
+print(array1+2)
+
+print()
+
+array1 = np.array(list1)
+print(array1-2)
+
